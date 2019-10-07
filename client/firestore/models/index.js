5d8b93ddbe2429cc36bb6414dd896ca35b9cdf9c
M. Wislek <mike.wislek@gmail.com>
2019-10-07 10:06:39 -0500

    Database seed and initial database API
    * Added database seed
    * Updated package.json with a new seed option
    * Created database API to make Firestore changes easier

---
+++ client/firestore/models/index.js
@@ -0,0 +1,9 @@
+const Instrument = require('./Instrument')
+const Project = require('./Project')
+const Timeslice = require('./Timeslice')
+
+module.exports = {
+  Instrument,
+  Project,
+  Timeslice
+}
