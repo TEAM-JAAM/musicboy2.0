a232645d2a2281d0ba784e6bd9b8d39999d92be1
Andre Birkus <birkusandre@gmail.com>
2019-10-10 10:56:34 -0500

    grid class updates

--- utils.js
+++ utils.js
@@ -11,68 +11,9 @@ export class AudioNode {
   }
 }
 
-export class Grid {
-  constructor(slices, key, instrument) {
-    this.slices = slices
-    this.key = key
-    this.instrument = instrument
-  }
-
-  setUpGrid(singleInstrument) {
-    let musicArray = []
-    for (let i = 0; i < this.slices; ++i) {
-      musicArray.push([])
-      for (let j = 0; j < 12; ++j) {
-        let node = new AudioNode(i, j, this.key[j], this.instrument)
-        musicArray[i].push(node)
-      }
-    }
-    return musicArray
-  }
-
-  //   addNewTimesliceBlock = () => {
-  //     console.log('INSTRUMENT: trying to add a new timeslice block to the grid')
-  //   }
-  //   removeTimesliceBlock = () => console.log('INSTRUMENT: trying to remove a timeslice block')
-  //   setKey = (key) => {
-  //     this.key = key
-  //     console.log('INSTRUMENT: trying to set the key to: ', key)
-  //   }
-  //   setCell = (row, col, value) => {
-  //     console.log('INSTRUMENT: changing the value of: (', row, ',', col, ') to ', value)
-  //   }
-}
-
 export const toggleCell = cell => {
   if (!cell.status) synth.triggerAttackRelease(cell.pitch, '16n')
   cell.status = !cell.status
-}
-
-export const createNewSequence = row => {
-  const seq = new Tone.Sequence(
-    function(time, note) {
-      synth.triggerAttackRelease(note, '32n', time)
-    },
-
-    row.reduce((accum, node) => {
-      if (node.status) accum.push(node.pitch)
-      else accum.push(0)
-      return accum
-    }, []),
-    '4n'
-  ).start(0)
-  return seq
-}
-
-export const updateSequences = (sequencesArray, row, rowIdx) => {
-  return sequencesArray.map((sequence, idx) => {
-    if (idx === rowIdx) {
-      sequence.cancel()
-      return createNewSequence(row)
-    } else {
-      return sequence
-    }
-  })
 }
 
 export const addRowToGrid = grid => {
@@ -99,4 +40,80 @@ export const startMusic = () => {
 
 export const stopMusic = () => {
   Tone.Transport.stop()
+}
+
+export class Grid {
+  constructor(collectionOfSlices, key, instrument) {
+    this.slices = collectionOfSlices
+    this.key = key
+    this.instrument = instrument
+    this.grid = this.setUpGrid(this.slices)
+    this.sequence = this.setUpSequence()
+  }
+
+  setUpGrid(slices) {
+    let nodeArray = []
+    for (let i = 0; i < slices.length; ++i) {
+      let booleanArray = Object.entries(slices[i].data())
+      nodeArray.push([])
+      for (let j = 0; j < 12; ++j) {
+        let node = new AudioNode(j, i, this.key[j], this.instrument)
+        if (booleanArray[j]) {
+          node.status = true
+        }
+        nodeArray[j].push(node)
+      }
+    }
+    return nodeArray
+  }
+
+  setUpSequence() {
+    let chordSequence = this.grid.map(slice => {
+      return slice.map(node => {
+        if (node.status) {
+          return node.pitch
+        }
+      })
+    })
+    return createNewSequence(chordSequence)
+  }
+
+  createNewSequence(arr) {
+    const seq = new Tone.Sequence(
+      function(time, note) {
+        synth.triggerAttackRelease(note, '32n', time)
+      },
+
+      row.reduce((accum, node) => {
+        if (node.status) accum.push(node.pitch)
+        else accum.push(0)
+        return accum
+      }, []),
+      '4n'
+    ).start(0)
+    return seq
+  }
+
+  updateSequences = (sequencesArray, row, rowIdx) => {
+    return sequencesArray.map((sequence, idx) => {
+      if (idx === rowIdx) {
+        sequence.cancel()
+        return createNewSequence(row)
+      } else {
+        return sequence
+      }
+    })
+  }
+
+  addNewTimesliceBlock() {
+    console.log('INSTRUMENT: trying to add a new timeslice block to the grid')
+  }
+  //   removeTimesliceBlock = () => console.log('INSTRUMENT: trying to remove a timeslice block')
+  //   setKey = (key) => {
+  //     this.key = key
+  //     console.log('INSTRUMENT: trying to set the key to: ', key)
+  //   }
+  //   setCell = (row, col, value) => {
+  //     console.log('INSTRUMENT: changing the value of: (', row, ',', col, ') to ', value)
+  //   }
 }
